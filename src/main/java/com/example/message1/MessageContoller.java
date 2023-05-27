e2c167f370657e759bff2f2a6c25ed8d60312011
VANOREK Yessica <yessica.vanorek@gmail.com>
2023-05-27 12:21:01 +0200

    summenfunktion hinzugefügt

--- src/main/java/com/example/message1/MessageContoller.java
+++ src/main/java/com/example/message1/MessageContoller.java
@@ -1,7 +1,14 @@
 package com.example.message1;
 
-public class MessageContoller {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+public class MessageContoller {
+    
+    @RequestMapping("/calc/sum")                                  // mit @RequestMapping ändert/erstellt man den Pfad der Webseite -->http://localhost:8080/calc/sum
+    public int sum (@RequestParam int a, @RequestParam int b){      // http://localhost:8080/calc/sum?a=3&b=4
+        return a+b;
+    }
 
 
 }
